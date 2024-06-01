561176e89aae81eb59b46a28b0e0bb7bfa237227
Zach W <zwells147@gmail.com>
2024-06-01 17:03:45 -0600

    Hello Triangle!

--- main.py
+++ main.py
@@ -2,12 +2,83 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import numpy as np
+
+# Vertex Shader
+v_shader = """
+#version 330
+layout(location = 0) in vec4 position;
+void main()
+{
+    gl_Position = position;
+}
+"""
+
+# Fragment Shader
+f_shader = """
+#version 330
+out vec4 fragColor;
+void main()
+{
+    fragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+}
+"""
+
+# Define the vertices of the triangle
+vertices = np.array([
+    [-0.5, -0.5, 0.0],  # Vertex 1
+    [0.5, -0.5, 0.0],   # Vertex 2
+    [0.0, 0.5, 0.0]     # Vertex 3
+], dtype=np.float32)
+
+# Create shader program
+def create_shader_program(vertex_shader, fragment_shader):
+    # Create the vertex shader
+    vertex_shader_id = glCreateShader(GL_VERTEX_SHADER)
+    glShaderSource(vertex_shader_id, vertex_shader)
+    glCompileShader(vertex_shader_id)
+
+    # Create the fragment shader
+    fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER)
+    glShaderSource(fragment_shader_id, fragment_shader)
+    glCompileShader(fragment_shader_id)
+
+    # Create the shader program
+    shader_program = glCreateProgram()
+    glAttachShader(shader_program, vertex_shader_id)
+    glAttachShader(shader_program, fragment_shader_id)
+    glLinkProgram(shader_program)
+
+    # Delete the shaders as they're linked into our program now and no longer necessery
+    glDeleteShader(vertex_shader_id)
+    glDeleteShader(fragment_shader_id)
+
+    return shader_program
+
+shader = create_shader_program(v_shader, f_shader)
 
 
 def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    # Create a vertex buffer object (VBO)
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+    # Create a vertex array object (VAO)
+    VAO = glGenVertexArrays(1)
+    glBindVertexArray(VAO)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+    glEnableVertexAttribArray(0)
+
+    # Unbind VAO
+    glBindVertexArray(0)
+
+    # Unbind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
 
     while True:
         for event in pygame.event.get():
@@ -18,7 +89,9 @@ def main():
         
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         # Draw your OpenGL objects here
-
+        glUseProgram(shader)
+        glBindVertexArray(VAO)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
         pygame.display.flip()
 
 if __name__ == '__main__':
